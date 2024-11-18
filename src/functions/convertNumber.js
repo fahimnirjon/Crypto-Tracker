ae6777fc2b2dab5851fd694c3c0329019fca5e82
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-18 22:40:42 +0600

    added

--- src/functions/convertNumber.js
+++ src/functions/convertNumber.js
@@ -10,7 +10,7 @@ export const convertNumber = (number) => {
   } else if (arr.length === 3) {
     // Millions
     return arr[0] + "." + arr[1].slice(0, 2) + "M";
-  } else if (arr.length == 2) {
+  } else if (arr.length === 2) {
     // Thousands
     return arr[0] + "." + arr[1].slice(0, 2) + "K";
   } else {
