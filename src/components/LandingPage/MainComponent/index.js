12795d75fb55476814da379a344298811009dfe9
fahimnirjon <fahimnirjon21@gmail.com>
2024-11-18 22:41:10 +0600

    added

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -47,8 +47,8 @@ function MainComponent() {
           </a>
           <RWebShare
             data={{
-              text: "CryptoDashboard made by Fahim using React JS.",
-              url: "https://crypto-dashboard-jan.netlify.app",
+              text: "CryptoDashboard made by Fahim Nirjon using React JS.",
+              url: "",
               title: "CryptoTracker.",
             }}
             onClick={() => toast.info("App Shared!")}
